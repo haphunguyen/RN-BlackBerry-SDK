31fa033393c01240b352ee46a32fc1a3140b1eeb
Nguyen Ha <phunguyen.2898@gmail.com>
2023-09-13 09:07:38 +0700

    add launcher

---
+++ modules/BlackBerry-Dynamics-for-React-Native-Launcher/ios/frameworks/BlackBerryLauncher.xcframework/ios-arm64_x86_64-simulator/BlackBerryLauncher.framework/Headers/BlackBerryLauncher.h
@@ -0,0 +1,9 @@
+//
+//  (c) 2020 BlackBerry Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <BlackBerryLauncher/GTLauncherViewController.h>
+#import <BlackBerryLauncher/GTLauncherSettingsViewController.h>
+#import <BlackBerryLauncher/GTLCommonSettingsCellConfiguration.h>
